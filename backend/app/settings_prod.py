76889c781430e1d442f7e8d49c49b8b90e3feb78
kvtodev <kvtodev@outlook.com>
2025-09-26 14:39:43 +0800

    make it

--- backend/app/settings_prod.py
+++ backend/app/settings_prod.py
@@ -1,15 +1,15 @@
 from app.settings import *
+import os
 
 DEBUG = False
 
-# TODO: 修改数据库连接的密码和 IP 地址
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "thss",
         "USER": "root",
-        "PASSWORD": "thss",
-        "HOST": "db",
+        "PASSWORD": os.environ.get("MYSQL_ROOT_PASSWORD", ""),
+        "HOST": "mysql",
         "PORT": "3306",
     }
 }
